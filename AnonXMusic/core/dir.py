61c51fe37d7b5da222faa892cb8a35651eb49d53
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-12-30 10:54:32 +0300

    Update dir.py

--- AnonXMusic/core/dir.py
+++ AnonXMusic/core/dir.py
@@ -1,20 +1,22 @@
 import os
-
-from ..logging import LOGGER
-
+import sys
+import logging
+from os import listdir, mkdir
 
 def dirr():
+    downloads_folder = "downloads"
+    cache_folder = "cache"
     for file in os.listdir():
-        if file.endswith(".jpg"):
-            os.remove(file)
-        elif file.endswith(".jpeg"):
-            os.remove(file)
-        elif file.endswith(".png"):
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
             os.remove(file)
 
-    if "downloads" not in os.listdir():
-        os.mkdir("downloads")
-    if "cache" not in os.listdir():
-        os.mkdir("cache")
+    if downloads_folder not in listdir():
+        mkdir(downloads_folder)
 
-    LOGGER(__name__).info("Directories Updated.")
+    if cache_folder not in listdir():
+        mkdir(cache_folder)
+
+    logging.info("Directories Updated.")
+
+if __name__ == "__main__":
+    dirr()
